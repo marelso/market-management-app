d09d7da525bbfb8aecb285785420b813230080c9
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-27 19:39:19 -0300

    Including constant file

---
+++ app/src/main/java/io/marelso/marketmanagement/data/Constant.kt
@@ -0,0 +1,12 @@
+package io.marelso.marketmanagement.data
+
+object Constant {
+
+    const val PAGE_SIZE: Int = 30
+
+    object MARKET_API {
+        const val URL: String = ""
+        const val NAME = "MARKET_API"
+    }
+
+}
