635144516e895f98eef8b399fc0172cc30ff54d4
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-27 19:25:07 -0300

    Updating routes system to implement interface

--- app/src/main/java/io/marelso/marketmanagement/navigation/AppNavGraph.kt
+++ app/src/main/java/io/marelso/marketmanagement/navigation/AppNavGraph.kt
@@ -2,24 +2,65 @@ package io.marelso.marketmanagement.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import io.marelso.marketmanagement.data.navigate
+import io.marelso.marketmanagement.ui.buy.BuyScreenHoisting
+import io.marelso.marketmanagement.ui.onboarding.OnBoardingScreenHoisting
+import io.marelso.marketmanagement.ui.sell.SellScreenHoisting
+import io.marelso.marketmanagement.ui.signin.SignInScreenHoisting
+import io.marelso.marketmanagement.ui.store.StoreWorkSpaceHoisting
+import io.marelso.marketmanagement.ui.stores.StoresScreenHoisting
 
 
 @Composable
 fun AppNavigationGraph(navHostController: NavHostController = rememberNavController()) {
     NavHost(navController = navHostController, startDestination = Routes.OnBoarding.route) {
         composable(route = Routes.OnBoarding.route) {
+            OnBoardingScreenHoisting(
+                onSignInClick = { navHostController.navigate(Routes.OnBoarding.navigate(Routes.SignIn)) },
+                onSignUpClick = { navHostController.navigate(Routes.OnBoarding.navigate(Routes.SignUp)) }
+            )
+        }
+
+        composable(route = Routes.SignIn.route) {
+            SignInScreenHoisting(
+                onSignInSuccess = {
+                    navHostController.navigate(Routes.SignIn.navigate(Routes.Stores))
+                }
+            )
+        }
+
+        composable(route = Routes.SignUp.route) {
 
         }
 
-        composable(route = Routes.Registration.route) {
+        composable(route = Routes.Stores.route) {
+            StoresScreenHoisting {
+                navHostController.navigate(Routes.Stores.navigate(Routes.Store,ARG_STORE_ID to it.id))
+            }
+        }
+        
+        composable(
+            route = Routes.Store.route,
+            arguments = listOf(navArgument(ARG_STORE_ID) { NavType.StringType })
+        ) { entry ->
+            val storeId: String = entry.arguments?.getString(ARG_STORE_ID).orEmpty()
 
+            StoreWorkSpaceHoisting {
+                navHostController.navigate(Routes.Store.navigate(it))
+            }
         }
 
-        composable(route = Routes.Home.route) {
+        composable(route = Routes.Buy.route) {
+            BuyScreenHoisting(onNavClick = navHostController::popBackStack)
+        }
 
+        composable(route = Routes.Sell.route) {
+            SellScreenHoisting()
         }
     }
 }
--- app/src/main/java/io/marelso/marketmanagement/navigation/Routes.kt
+++ app/src/main/java/io/marelso/marketmanagement/navigation/Routes.kt
@@ -1,17 +1,15 @@
 package io.marelso.marketmanagement.navigation
 
-sealed class Routes(val route: String) {
-    companion object {
-        fun navigate(to: Routes, vararg parameters: Pair<String, String>?): String {
-            return parameters.fold(to.route) { route, parameter ->
-                parameter?.let {
-                    route.replace("{${it.first}}", it.second)
-                } ?: route
-            }
-        }
-    }
+import io.marelso.marketmanagement.data.Route
 
+const val ARG_STORE_ID = "storeId"
+
+sealed class Routes(override val route: String): Route(route) {
     data object OnBoarding: Routes(route = "/onboarding")
-    data object Registration: Routes(route = "/registration")
-    data object Home: Routes(route = "/home")
+    data object SignIn: Routes(route = "/sign-in")
+    data object SignUp: Routes(route = "/sign-up")
+    data object Stores: Routes(route = "/stores")
+    data object Store: Routes(route = "/stores/{$ARG_STORE_ID}")
+    data object Buy: Routes(route = "/stores/{$ARG_STORE_ID}/buy")
+    data object Sell: Routes(route = "/stores/{$ARG_STORE_ID}/sell")
 }
