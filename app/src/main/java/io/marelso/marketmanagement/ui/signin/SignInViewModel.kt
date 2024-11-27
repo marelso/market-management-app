ccb9fd6e619464635cafce980bc4d2b315b733af
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-27 19:51:52 -0300

    Including reference to account repository at sign in viewmodel

---
+++ app/src/main/java/io/marelso/marketmanagement/ui/signin/SignInViewModel.kt
@@ -0,0 +1,7 @@
+package io.marelso.marketmanagement.ui.signin
+
+import androidx.lifecycle.ViewModel
+import io.marelso.marketmanagement.data.network.account.AccountRepository
+
+class SignInViewModel(private val repository: AccountRepository): ViewModel() {
+}
