d3bd2e95bbb802ab64322aa5778c11e6e8fe7939
Marcelo Klein Sousa Junior <marelso@Marcelos-MacBook-Air.local>
2024-11-30 03:45:12 -0300

    Including new route to product and navigating from stock screen

--- app/src/main/java/io/marelso/marketmanagement/ui/store/navigation/StoreNavGraph.kt
+++ app/src/main/java/io/marelso/marketmanagement/ui/store/navigation/StoreNavGraph.kt
@@ -6,7 +6,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import io.marelso.marketmanagement.data.navigate
+import io.marelso.marketmanagement.navigation.Routes
 import io.marelso.marketmanagement.ui.store.home.StoreHomeScreenHoisting
+import io.marelso.marketmanagement.ui.store.product.ProductScreenHoisting
 import io.marelso.marketmanagement.ui.store.stock.StoreStockScreenHoisting
 import org.koin.androidx.compose.koinViewModel
 
@@ -17,7 +20,15 @@ fun StoreNavGraph(
     NavHost(navController = navHostController, startDestination = StoreRoutes.Home.route) {
         composable(route = StoreRoutes.Home.route) { StoreHomeScreenHoisting() }
 
-        composable(route = StoreRoutes.Stock.route) { StoreStockScreenHoisting(koinViewModel()) }
+        composable(route = StoreRoutes.Stock.route) {
+            StoreStockScreenHoisting(koinViewModel()) {
+                navHostController.navigate(
+                    StoreRoutes.Stock.navigate(Routes.Store, ARG_PRODUCT_ID to it)
+                )
+            }
+        }
+
+        composable(route = StoreRoutes.Stock.route) { ProductScreenHoisting() }
 
         composable(route = StoreRoutes.Entries.route) { Text(text = "Entries") }
 
--- app/src/main/java/io/marelso/marketmanagement/ui/store/navigation/StoreRoutes.kt
+++ app/src/main/java/io/marelso/marketmanagement/ui/store/navigation/StoreRoutes.kt
@@ -3,6 +3,8 @@ package io.marelso.marketmanagement.ui.store.navigation
 import io.marelso.marketmanagement.R
 import io.marelso.marketmanagement.data.Route
 
+val ARG_PRODUCT_ID = "productId"
+
 sealed class StoreRoutes(
     override val route: String,
     val icon: Int,
@@ -10,6 +12,7 @@ sealed class StoreRoutes(
 ): Route(route) {
     data object Home : StoreRoutes(route = "/home", icon = R.drawable.ic_home)
     data object Stock : StoreRoutes(route = "/stock", icon = R.drawable.ic_stock)
+    data object Product : StoreRoutes(route = "/product/{$ARG_PRODUCT_ID}", icon = R.drawable.ic_stock)
     data object Entries : StoreRoutes(route = "/entries", icon = R.drawable.ic_entries)
     data object Settings : StoreRoutes(route = "/settings", icon = R.drawable.ic_notifications)
 }
